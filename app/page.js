9d7cf4b73961c63ed769a65f3ca86c4c9b586726
Pradum Singh <hs947518@gmail.com>
2024-12-19 03:47:59 +0000

    donee

--- app/page.js
+++ app/page.js
@@ -1,9 +1,20 @@
-import Image from "next/image";
+"use client";
 
+import Nav from "@/components/ProductUI/Nav/Nav";
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-    <h1>hello from ggit </h1>
+    <div className="flex flex-col w-full items-center justify-between min-h-screen p-8 gap-16  sm:p-20 font-[family-name:var(--font-geist-sans)] ">
+      <div>
+        <h1 className="text-7xl  text-pink-500  font-bold">
+          {" "}
+          Make your invo<span className="text-9xl">I</span>ce{" "}
+        </h1>
+        <p className=" absolute  right-72">
+          <span className="bg-yellow-500 text-4xl p-2 border-4 border-t-yellow-200">
+            N Second
+          </span>
+        </p>
+      </div>
     </div>
   );
 }
