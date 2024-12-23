8974b640f76a4210bb648ff46b3b337f7fdbd4ff
Pradum Singh <hs947518@gmail.com>
2024-12-23 13:06:20 +0000

    all

--- app/api/HashPassword/route.js
+++ app/api/HashPassword/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 //import argon2 from "argon2";
-import { db } from "@/lib/firebase";
+//import { db } from "@/lib/firebase";
 export async function POST(req) {
   try {
     const { fname, lname, email, password } = await req.json();
@@ -9,29 +9,30 @@ export async function POST(req) {
         { success: false, error: "All fields are required" },
         { status: 400 }
       );
-    } else {
-      const usersRef = db.collection("Users");
-      const snapshot = await usersRef.where("email", "==", email).get();
+    } //else {
+    // const usersRef = db.collection("Users");
+    //const snapshot = await usersRef.where("email", "==", email).get();
 
-      if (!snapshot.empty) {
-        return NextResponse.json({
-          success: "login",
-          msg: "You have an account ",
-        });
-      }
-    }
+    //  if (!snapshot.empty) {
+    //  return NextResponse.json({
+    //   success: "login",
+    //   msg: "You have an account ",
+    //  });
+    //   }
+    // }
 
     // Hash the password
     const hashedPassword = 12345; // await argon2.hash(password);
 
-    const docRef = await db.collection("Users").add({
+    /**
+    *  const docRef = await db.collection("Users").add({
       fname,
       lname,
       email,
       password: hashedPassword,
     });
     console.log("User added successfully:");
-
+*/
     return NextResponse.json({
       success: "regi",
       msg: "Welcome to Invoice",
