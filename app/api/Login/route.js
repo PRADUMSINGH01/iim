aa0bf6d42861ec3d79f5dca39b8d4090ace4968d
Pradum Singh <hs947518@gmail.com>
2024-12-23 13:17:41 +0000

    login

--- app/api/Login/route.js
+++ app/api/Login/route.js
@@ -1,16 +1,17 @@
 import { NextResponse } from "next/server";
-import { db } from "@/lib/firebase";
-import argon2 from "argon2";
+//import { db } from "@/lib/firebase";
+//import argon2 from "argon2";
 import jwt from "jsonwebtoken";
 export async function POST(res) {
   const JWT = process.env.JWT;
   const { email, password } = await res.json();
-  const usersRef = db.collection("Users");
-  const snapshot = await usersRef.where("email", "==", email).get();
-  const userDoc = snapshot.docs[0];
-  const data = userDoc.data();
-  const isPasswordValid = await argon2.verify(data.password, password);
+  //const usersRef = db.collection("Users");
+  //const snapshot = await usersRef.where("email", "==", email).get();
+  //const userDoc = snapshot.docs[0];
+  //const data = userDoc.data();
+  //const isPasswordValid = await argon2.verify(data.password, password);
 
+  /*
   if (!isPasswordValid) {
     return NextResponse.json({
       success: false,
@@ -29,5 +30,8 @@ export async function POST(res) {
     });
 
     // Set the cookie in the response
-  }
+  }*/
+  return NextResponse.json({
+    msg: "You are logged in",
+  });
 }
